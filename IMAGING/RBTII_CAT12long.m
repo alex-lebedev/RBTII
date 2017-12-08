b93b90749120ba1b6bbec1a2b15e9a8f39391fb6
Alex Lebedev <alexander.vl.lebedev@gmail.com>
2017-12-08 11:55:43 +0100

    Create RBTII_CAT12long.m

---
+++ IMAGING/RBTII_CAT12long.m
@@ -0,0 +1,32 @@
+%-----------------------------------------------------------------------
+% Job saved on 08-Dec-2017 11:51:01 by cfg_util (rev $Rev: 6460 $)
+% spm SPM - SPM12 (6685)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.tools.cat.tools.long.subj(1).mov = {
+                                                       '/home/ALDRECENTRUM/alexander.lebedev/Desktop/RBTII_data/NIFTI/sub-1001/ses-pretrain/anat/sub-1001_ses-pretrain_T1w.nii,1'
+                                                       '/home/ALDRECENTRUM/alexander.lebedev/Desktop/RBTII_data/NIFTI/sub-1001/ses-posttrain/anat/sub-1001_ses-posttrain_T1w.nii,1'
+                                                       };
+matlabbatch{1}.spm.tools.cat.tools.long.subj(2).mov = {
+                                                       '/home/ALDRECENTRUM/alexander.lebedev/Desktop/RBTII_data/NIFTI/sub-1017/ses-pretrain/anat/sub-1017_ses-pretrain_T1w.nii,1'
+                                                       '/home/ALDRECENTRUM/alexander.lebedev/Desktop/RBTII_data/NIFTI/sub-1017/ses-posttrain/anat/sub-1017_ses-posttrain_T1w.nii,1'
+                                                       };
+matlabbatch{1}.spm.tools.cat.tools.long.subj(3).mov = {
+                                                       '/home/ALDRECENTRUM/alexander.lebedev/Desktop/RBTII_data/NIFTI/sub-1033/ses-pretrain/anat/sub-1033_ses-pretrain_T1w.nii,1'
+                                                       '/home/ALDRECENTRUM/alexander.lebedev/Desktop/RBTII_data/NIFTI/sub-1033/ses-posttrain/anat/sub-1033_ses-posttrain_T1w.nii,1'
+                                                       };
+matlabbatch{1}.spm.tools.cat.tools.long.opts.tpm = {'/home/ALDRECENTRUM/alexander.lebedev/Desktop/tbx/spm12/tpm/TPM.nii'};
+matlabbatch{1}.spm.tools.cat.tools.long.opts.affreg = 'mni';
+matlabbatch{1}.spm.tools.cat.tools.long.opts.biasstr = 0.5;
+matlabbatch{1}.spm.tools.cat.tools.long.extopts.APP = 2;
+matlabbatch{1}.spm.tools.cat.tools.long.extopts.LASstr = 0.5;
+matlabbatch{1}.spm.tools.cat.tools.long.extopts.gcutstr = 0.5;
+matlabbatch{1}.spm.tools.cat.tools.long.extopts.cleanupstr = 0.5;
+matlabbatch{1}.spm.tools.cat.tools.long.extopts.registration.darteltpm = {'/home/ALDRECENTRUM/alexander.lebedev/Desktop/tbx/spm12/toolbox/cat12/templates_1.50mm/Template_1_IXI555_MNI152.nii'};
+matlabbatch{1}.spm.tools.cat.tools.long.extopts.registration.shootingtpm = {'/home/ALDRECENTRUM/alexander.lebedev/Desktop/tbx/spm12/toolbox/cat12/templates_1.50mm/Template_0_IXI555_MNI152_GS.nii'};
+matlabbatch{1}.spm.tools.cat.tools.long.extopts.registration.regstr = 4;
+matlabbatch{1}.spm.tools.cat.tools.long.extopts.vox = 1.5;
+matlabbatch{1}.spm.tools.cat.tools.long.output.surface = 1;
+matlabbatch{1}.spm.tools.cat.tools.long.modulate = 1;
+matlabbatch{1}.spm.tools.cat.tools.long.warps = 0;
+matlabbatch{1}.spm.tools.cat.tools.long.dartel = 2;
